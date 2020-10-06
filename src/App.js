d2dc8946d1d9c0997444195e0745a41866185637
Stephen Guo <wetech16@gmail.com>
2020-10-06 16:10:43 -0700

    -updated

--- src/App.js
+++ src/App.js
@@ -1,42 +1,46 @@
 // Dependancies
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import jwtDecode from 'jwt-decode';
-import axios from 'axios';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import jwtDecode from "jwt-decode";
+import axios from "axios";
 // Components
-import Navbar from './components/layout/Navbar';
-import AuthRoute from './util/AuthRoute';
+import Navbar from "./components/layout/Navbar";
+import AuthRoute from "./util/AuthRoute";
 // Pages
-import Home from './pages/Home';
-import login from './pages/Login';
-import signup from './pages/Signup';
-import user from './pages/User';
+import Home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
+import user from "./pages/user";
 // Redux
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import { SET_AUTHENTICATED } from './redux/types';
-import { logOutUser, getUserData } from './redux/actions/userActions';
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { SET_AUTHENTICATED } from "./redux/types";
+import { logOutUser, getUserData } from "./redux/actions/userActions";
 // MUI stuff
-import './App.css';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { global__theme } from './util/theme';
-
+import "./App.css";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { global__theme } from "./util/theme";
 
 const theme = createMuiTheme(global__theme);
 
-axios.defaults.baseURL = 'https://europe-west6-screams-62f3b.cloudfunctions.net/api';
+axios.defaults.baseURL =
+  "https://europe-west6-screams-62f3b.cloudfunctions.net/api";
 
-const token = localStorage.getItem('FBIdToken');
+const token = localStorage.getItem("FBIdToken");
 if (token) {
   const decodedToken = jwtDecode(token);
   // console.log(decodedToken); expired after 1 hour
   if (decodedToken.exp * 1000 < Date.now()) {
     store.dispatch(logOutUser());
-    window.location.href = '/login';
+    window.location.href = "/login";
   } else {
     store.dispatch({ type: SET_AUTHENTICATED });
-    axios.defaults.headers.common['Authorization'] = token;
+    axios.defaults.headers.common["Authorization"] = token;
     store.dispatch(getUserData());
   }
 }
@@ -44,23 +48,25 @@ const App = () => {
   return (
     <MuiThemeProvider theme={theme}>
       <Provider store={store}>
-
         <Router>
           <Navbar />
-          <div className='container'>
+          <div className="container">
             <Switch>
-              <Route exact path='/' component={Home} />
-              <AuthRoute exact path='/login' component={login} />
-              <AuthRoute exact path='/signup' component={signup} />
-              <Route exact path='/users/:handle' component={user} />
-              <Route exact path='/users/:handle/scream/:screamId' component={user} />
+              <Route exact path="/" component={Home} />
+              <AuthRoute exact path="/login" component={login} />
+              <AuthRoute exact path="/signup" component={signup} />
+              <Route exact path="/users/:handle" component={user} />
+              <Route
+                exact
+                path="/users/:handle/scream/:screamId"
+                component={user}
+              />
             </Switch>
           </div>
         </Router>
-
       </Provider>
     </MuiThemeProvider>
   );
-}
+};
 
 export default App;
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,35 +1,37 @@
 // dependencies
-import React, { useState, useEffect } from 'react'
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import appIcon from './../images/icon.png';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import appIcon from "./../images/icon.png";
 // MUI stuff
-import withStyles from '@material-ui/core/styles/withStyles';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { signup__theme } from '../util/theme';
+import withStyles from "@material-ui/core/styles/withStyles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { signup__theme } from "../util/theme";
 // Redux stuff
-import { connect } from 'react-redux';
-import { signupUser } from '../redux/actions/userActions';
+import { connect } from "react-redux";
+import { signupUser } from "../redux/actions/userActions";
 
 const styles = { ...signup__theme };
 
 const Signup = (props) => {
+  const {
+    classes,
+    signupUser,
+    UI: { loading },
+  } = props;
 
-  const { classes, signupUser, UI: { loading } } = props;
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [handle, setHandle] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [handle, setHandle] = useState("");
   const [errors, setErrors] = useState({});
 
   useEffect(() => {
-    if (props.UI.errors)
-      setErrors(props.UI.errors);
+    if (props.UI.errors) setErrors(props.UI.errors);
   }, [props]);
 
   const handleSubmit = (e) => {
@@ -38,111 +40,129 @@ const Signup = (props) => {
       email,
       password,
       confirmPassword,
-      handle
+      handle,
     };
     signupUser(newUserData, props.history);
-  }
+  };
 
   return (
     <Grid container className={classes.form}>
       <Grid item sm />
-      <Grid item sm >
-        <img src={appIcon} alt='Screams Social Ape' className={classes.image} />
-        <Typography variant='h2' className={classes.pageTitle} >
+      <Grid item sm>
+        <img
+          src={appIcon}
+          alt="Screams Social Ape"
+          className={classes.image}
+        />
+        <Typography variant="h2" className={classes.pageTitle}>
           Signup
-          </Typography>
+        </Typography>
         <form noValidate onSubmit={handleSubmit}>
           <TextField
-            id='email'
-            name='email'
-            type='email'
-            label='Email'
+            id="email"
+            name="email"
+            type="email"
+            label="Email"
             className={classes.textField}
             helperText={errors.email}
             error={errors.email ? true : false}
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            fullWidth />
+            fullWidth
+          />
           <TextField
-            id='password'
-            name='password'
-            type='password'
-            label='Password'
+            id="password"
+            name="password"
+            type="password"
+            label="Password"
             className={classes.textField}
             helperText={errors.password}
-            error={errors.password ? true : false} value={password}
+            error={errors.password ? true : false}
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
-            fullWidth />
+            fullWidth
+          />
           <TextField
-            id='confirmPassword'
-            name='confirmPassword'
-            type='password'
-            label='Confirm Password'
+            id="confirmPassword"
+            name="confirmPassword"
+            type="password"
+            label="Confirm Password"
             className={classes.textField}
             helperText={errors.confirmPassword}
-            error={errors.confirmPassword ? true : false} value={confirmPassword}
+            error={errors.confirmPassword ? true : false}
+            value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
-            fullWidth />
+            fullWidth
+          />
           <TextField
-            id='handle'
-            name='handle'
-            type='text'
-            label='handle'
+            id="handle"
+            name="handle"
+            type="text"
+            label="handle"
             className={classes.textField}
             helperText={errors.handle}
-            error={errors.handle ? true : false} value={handle}
+            error={errors.handle ? true : false}
+            value={handle}
             onChange={(e) => setHandle(e.target.value)}
-            fullWidth />
+            fullWidth
+          />
 
           {errors.general && (
             <Typography
-              variant='body2' className={classes.customeError}
+              variant="body2"
+              className={classes.customeError}
             >
               {errors.general}
             </Typography>
           )}
 
           <Button
-            variant='contained'
-            type='submit'
-            color='primary'
+            variant="contained"
+            type="submit"
+            color="primary"
             className={classes.button}
             disabled={loading}
           >
             Sign up
-              {loading && (
+            {loading && (
               <CircularProgress
-                className={classes.progress} size={30} />
+                className={classes.progress}
+                size={30}
+              />
             )}
-          </Button><br />
-          <small>Already have an account ? log in <Link to='/login'>here</Link></small>
+          </Button>
+          <br />
+          <small>
+            Already have an account ? log in{" "}
+            <Link to="/login">here</Link>
+          </small>
         </form>
       </Grid>
       <Grid item sm />
     </Grid>
   );
-}
+};
 
 // PropTypes
 Signup.protoTypes = {
   classes: PropTypes.object.isRequired,
   signupUser: PropTypes.func.isRequired,
   user: PropTypes.object.isRequired,
-  UI: PropTypes.object.isRequired
+  UI: PropTypes.object.isRequired,
 };
 
 // Pull state from Redux Store To Component
 const mapStateToProps = (state) => ({
   user: state.user,
-  UI: state.UI
+  UI: state.UI,
 });
 
 // Push Actions To Props
 const mapActionsToProps = {
-  signupUser
+  signupUser,
 };
 
 export default connect(
   mapStateToProps,
-  mapActionsToProps)
-  (withStyles(styles)(Signup));
+  mapActionsToProps
+)(withStyles(styles)(Signup));
